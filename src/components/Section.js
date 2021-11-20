01ae68ff314767cb4a120d4719a9ed4b79c7b601
JOAT <joat@JOATs-iMac-Pro.attlocal.net>
2021-11-19 22:07:05 -0500

    added image assets and carInfo.json

---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Section() {
+    return (
+        <Wrap>
+            Section
+        </Wrap>
+    )
+}
+
+const Wrap = styled.div``
