0fc5b536cce3ef4993844eb07f9728157b7c851c
JOAT <joat@JOATs-iMac-Pro.attlocal.net>
2021-11-20 20:17:14 -0500

    added Fade component to text and button

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,21 +5,25 @@ import Fade from 'react-reveal/Fade'
 export default function Section({ title, description, backgroundImg, leftBtnText, rightBtnText}) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{ title }</h1>
-                <p>{ description }</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{ title }</h1>
+                    <p>{ description }</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        { leftBtnText }
-                    </LeftButton>
-                    { rightBtnText && 
-                        <RightButton>
-                            { rightBtnText }
-                        </RightButton>
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            { leftBtnText }
+                        </LeftButton>
+                        { rightBtnText && 
+                            <RightButton>
+                                { rightBtnText }
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/images/down-arrow.svg" />
             </Buttons>
         </Wrap>
