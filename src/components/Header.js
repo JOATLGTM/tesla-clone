0c0069c4b2187a44c9bec046dc3a4956f628ed8b
JOAT <joat@JOATs-iMac-Pro.attlocal.net>
2021-11-21 13:37:19 -0500

    added nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import CloseButton from 'react-bootstrap/CloseButton'
 
 export default function Header() {
+
+    let sideMenus = ['Existing Inventory', 'Used Inventory', 'Trade-In', 'Test Drive', 'Cybertruck',
+    'Roadster', 'Semi', 'Charging', 'Powerwall', 'Commercial Energy', 'Utilities', 'Find Us', 
+    'Support' ,'Investor Relations'];
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
     return (
         <div>
            <Container>
@@ -18,11 +26,19 @@ export default function Header() {
                     <a href="#">Solar Roof</a>
                     <a href="#">Solar Panels</a>
                 </Menu>
-                <RightMenu>
+                <RightMenu onClick={() => setIsMenuOpen(true)}>
                     <a href="#">Shop</a>
                     <a href="#">Account</a>
                     <a href="#">Menu</a>
                 </RightMenu>
+                <Nav show={isMenuOpen}> 
+                    <CustomWrapper>
+                        <CloseButton onClick={() => setIsMenuOpen(false)}/>
+                    </CustomWrapper>
+                    <li>
+                        {sideMenus.map(menu => <a href="#">{menu}</a>)}
+                    </li>
+                </Nav>
            </Container>
         </div>
     )
@@ -42,6 +58,7 @@ const Container = styled.div`
     top: 0;
     left: 0;
     right: 0;
+    z-index: 1;
 `
 
 const Menu = styled.div`
@@ -69,4 +86,40 @@ const RightMenu = styled.div`
         margin-right: 10px
         flex-wrap: no-wrap;
     }
+`
+
+const Nav = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    width: 300px;
+    z-index: 16;
+    list-style: none;
+    li {
+        padding: 20px;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+
+        a {
+            margin: 10px;
+            font-weight: 600;
+        }
+    }
+    transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)' };
+    transition: transform 0.2s;
+`
+
+const CustomWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    padding: 15px;
+`
+
+const CustomMenu = styled.div`
+    font-weight: 600;
+    color: black;
+    font-weight: 800 !important;                                     
 `
